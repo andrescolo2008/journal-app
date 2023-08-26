f91c6f2bf29b541ab91ad565abacda70c119e806
Andres Correa <andrescolo2008gmail.com>
2023-08-26 15:04:44 -0500

    2.interface login

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,11 +1,15 @@
 import { AppRouter } from "./router/AppRouter"
-
+import { AppTheme } from "./theme/AppTheme"
+AppTheme
 export const JournalApp = () => {
   return (
 
     <>
-  
+  <AppTheme>
+
     <AppRouter/>
+
+  </AppTheme>
     
 
     </>
