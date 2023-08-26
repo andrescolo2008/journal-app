28c0efc632bc6f68505ca366e6d132a534151e99
Andres Correa <andrescolo2008gmail.com>
2023-08-26 15:35:38 -0500

    3.AuthLayout-formato interface usuario

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,24 +3,14 @@ import { Google } from '@mui/icons-material'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
+import { AuthLayouth } from '../layout/AuthLayouth'
 
 export const LoginPage = () => {
   return (
     
-    <Grid
-    container
-    spacing={0}
-    direction="column"
-    alignItems="center"
-    justifyContent="center"
-    sx={{minHeight:'100vh',backgroundColor:'primary.main', padding:4}}
-    >
-      <Grid item 
-      className='box-shadow'
-      xs={3}
-      sx={{ backgroundColor:'white',padding:3,borderRadius:2}}  >
-        <Typography variant='h5' sx={{mb:1}} > Login</Typography>
-               <form >
+    <AuthLayouth title='Login'>
+
+            <form >
                     <Grid container>
                           <Grid item xs={12} sx={{mt:2}} >
                               <TextField 
@@ -64,9 +54,9 @@ export const LoginPage = () => {
 
                     </Grid>
                </form>
-         </Grid >
 
-      </Grid >
+    </AuthLayouth>
+              
     
   )
 }
