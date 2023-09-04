d714ad29280933fee2334295dad9f31614a32c83
Andres Correa <andrescolo2008gmail.com>
2023-09-04 14:49:09 -0500

    2.parte-1.config store y authslice

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,23 @@ import React from 'react'
 import './styles.css'
 import { JournalApp } from './JournalAPP'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+<Provider store={store}>   
+
           <BrowserRouter>
+
                  <JournalApp />
 
+
           </BrowserRouter>
+
+</Provider>
 
   </React.StrictMode>,
 )
