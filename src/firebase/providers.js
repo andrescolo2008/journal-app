c0534e9503ac87672d8e8cb6e3d065401011c870
Andres Correa <andrescolo2008gmail.com>
2023-09-18 14:29:10 -0500

    16.animaciones-Animate.css

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,11 +1,13 @@
 import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
+const googleProvider = new GoogleAuthProvider();
+
   export const signInWithGoogle = async ( ) =>{
 
     try{
         const result = await signInWithPopup(FirebaseAuth,googleProvider)
-        const credentials= GoogleAuthProvider.credentialFromResult(result)
+        // const credentials= GoogleAuthProvider.credentialFromResult(result)
 
         const {displayName,email,photoURL,uid} = result.user
         console.log({displayName , email,photoURL,uid});
